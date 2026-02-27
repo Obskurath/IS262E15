7c88eef6e4d8462f7bbcfb9d7b5578427595a1c1
Ghost_Monkey <deltanueve738@gmail.com>
2026-02-26 19:16:14 -0600

    Add initial project assets and configuration files for WebApp262E15
    - Created WebApp262E15.csproj.nuget.g.props to define NuGet properties.
    - Added WebApp262E15.csproj.nuget.g.targets for build targets.
    - Generated project.assets.json to manage project dependencies and assets.
    - Introduced project.nuget.cache for caching NuGet package information.

---
+++ src/Application262E15/Class1.cs
@@ -0,0 +1,12 @@
+﻿public class Figura262E15 {
+public static double Perimetro(double lado, double numeroLados) {
+return lado * numeroLados;
+}
+public static double Area(double lado, double apotema, double numeroLados) {
+double perimetro = Perimetro(lado, numeroLados);
+return perimetro * apotema/2;
+}
+public static double Volumen(double area, double altura) {
+return area * altura;
+}
+}
---
+++ src/ConsoleApp262E15/Program.cs
@@ -0,0 +1,12 @@
+﻿//Formula de un Poligono regular 
+//Variables
+double lado = 5;
+double apotema = 3.5;
+double numeroLados = 6;
+double altura = 10;
+double perimetro = Figura262E15.Perimetro (lado,numeroLados);
+double area = Figura262E15.Area(perimetro,apotema,numeroLados);
+double volumen =Figura262E15.Volumen(area,altura);     
+
+//Salida de datos
+Console.WriteLine($"El perimetro de un Poligono regular con un lados de {lado} cm pero teniendo {numeroLados} lados, tiene un perimetro de {perimetro} cm\nEl area de de un Poligono Regular con un perimetro de {perimetro} cm y un apotema de {apotema} cm, su area es de {area} cm2\nEl volumen de un poligono regular con un area de {area} cm2 y una altura de {altura} cm es de {volumen} cm3");
